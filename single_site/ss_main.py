87908597af3ba4ccf7a134db84e5a9783421afbd
sarasame00 <sarasame00@gmail.com>
2025-05-14 11:58:55 +0200

    Upload presets

--- single_site/ss_main.py
+++ single_site/ss_main.py
@@ -11,7 +11,9 @@ from single_site.ss_runner import run_all_ss_simulations
 PRESET = "3d_d1_r2"         # Choose from: '3d_d1_r1', '4d_d1_r1', '5d_d1_r1', etc.
 RESOLUTION = 3           # Parameter resolution (per dimension)
 
-presets = ["3d_d1_r2","4d_d1_r2","5d_d1_r2"]
+
+# !!!!!!!! PARALLEL EXECUTATION NOT WORKING FOR SS !!!!!!!!!
+presets = ["3d_d2_r1","4d_d2_r1","5d_d2_r1"]
 if __name__ == "__main__":
     for pres in presets:
         param_tuples = generate_ss_grid(pres, RESOLUTION)
